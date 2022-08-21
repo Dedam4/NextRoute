168f9143f928f2597519a9175268ca8dc064407f
Dedam4 <shtrak2014@mail.ru>
2022-08-21 18:29:08 +0300

    Project structure

---
+++ route/hooks/useFilter.ts
@@ -0,0 +1,9 @@
+import { IOptions } from "../types/IOptions";
+import { IPost } from "../types/IPost";
+
+
+
+export function useFilter(posts: IPost[], { sort = 'ASC', filter }: IOptions)  {
+    
+    
+}
---
+++ route/pages/api/post.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { IPost } from '../../types/IPost';
+import posts from '../api/_post/posts.json';
+
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<IPost[]>
+) {
+    if (req.method === 'GET') res.status(200).json(posts);
+}
